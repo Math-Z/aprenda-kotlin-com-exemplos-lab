840d3585bf8f48e09ca1000c221e3938cb0806a5
HypeerZ <matheusrmds@gmail.com>
2023-09-29 16:40:25 -0300

    DIO - Desafio Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,45 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val idade: Int)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val professor: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional> ) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} foi matriculado em ${nome} com sucesso.")
+    }
+
+    fun status(){
+        println("Curso: ${nome}")
+        println("Nivel: ${nivel}")
+        for (c in conteudos){
+            println("Conteudo:")
+            println("   Materia: ${c.nome}")
+            println("   Professor: ${c.professor}")
+            println("   Duracao: ${c.duracao}h")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val user1 = Usuario("Matheus", 19)
+    val user2 = Usuario("Andre", 19)
+
+    val conteudo1 = ConteudoEducacional("Kotlin Orientado a Objetos", "Professor X", 3)
+    val conteudo2 = ConteudoEducacional("Introducao a Python", "Professor N", 1)
+
+    val formacao1 = Formacao("Curso de Kotlin", Nivel.INTERMEDIARIO, listOf(conteudo1))
+    val formacao2 = Formacao("Curso de Python", Nivel.BASICO, listOf(conteudo2))
+
+    formacao1.matricular(user1)
+    formacao1.status()
+
+    println()
+
+    formacao2.matricular(user2)
+    formacao2.status()
 }
